4e43f0ef387e7aa426985bc2032c6076d601bdcf
Joel Kerfoot <kerfootj@linux3.csc.uvic.ca>
2018-05-29 12:17:20 -0700

    sh360 commit 1

--- a1/sh360.c
+++ a1/sh360.c
@@ -1,13 +1,28 @@
+/*
+ * Joel Kerfoot
+ * CSC 360 A1
+ */
+
+// Max # args: 7
+// Max len input: 80 chars
+// Max len promt: 10 chars
+// Max # direct: 10
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-void sh360()
+
+
+	
+int main(int argc, char *argv[]) 
 {
-	
-	int main(int argc, char *argv[]) 
-	{
-	
+	for(;;){
+		
 	}
-
+	
 }
+
+
