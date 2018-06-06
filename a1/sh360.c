f12c033e232af3c1320fd620f357c1cd9aa10a10
kerfootj <joelkerfoot@gmail.com>
2018-06-06 01:47:56 -0700

    reworking sh360.c

--- a1/sh360.c
+++ a1/sh360.c
@@ -3,381 +3,219 @@
  * CSC 360 A1
  */
 
+/******************************* REFERENCES ******************************/
+//
+// [1] https://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
+//
+
+/******************************* LIBRARIES *******************************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdbool.h>
 #include <fcntl.h>
 
-#define MAX_INPUT_LINE 80
-#define INPUT_BUFFER 80
-#define MAX_NUM_TOKENS 27
-#define MAX_NUM_ARGS 7
-#define MAX_NUM_DIRECTORIES 10
-#define MAX_LEN_PATH 80
-#define MAX_LEN_PROMT 10
+/******************************* CONSTANTS *******************************/
+
+#define MAX_NUM_ARGS 10 // 7
+#define MAX_LINE_LENGTH 80
+#define MAX_PROMPT_LENGTH 10
+#define MAX_NUM_DIRS_IN_PATH 10
 
 
-void command_positions(int *arr, char **token, int len) {
-	int n = 1;	
-	for(int i = 1; i < len; i++) {
-		if(strcmp(token[i], "->") == 0){
-			arr[n++] = i+1;
-		} 	
-	}
-}
+/************************** GLOBAL VARIABLES ****************************/
 
-void process_args(char *args[], char **token, int start, int end) {
-	for(int i = 0; i < MAX_NUM_ARGS; i++) {
-		if(i < end - start)
-			args[i] = token[start+i];
-		else
-			args[i] = NULL;
-	}
-}
+char g_prompt[MAX_PROMPT_LENGTH];
+char g_dirs[MAX_NUM_DIRS_IN_PATH][MAX_LINE_LENGTH];
 
-bool validate_command(char **cmd) {
+int g_numDirs;
 
-	if(*cmd[strlen(*cmd) - 1] == '\n') {
-		*cmd[strlen(*cmd) - 1] = '\0';
-	}
+/****************************** PROTOTYPES ******************************/
 
-	struct stat file_stat;
-	
-	FILE *rc;
-	rc = fopen(".sh360rc", "r");
+int read_config();
+int read_input(char *input);
 
-	char filename[MAX_LEN_PATH];
-	int n = 0;
+int tokenize_string(char **token, char *cmd);
+int find_path(char *binary, char *fullpath);
 
-	while(fgets(filename, sizeof filename, rc) != NULL) {
-		if(n > 0 && n <= MAX_NUM_DIRECTORIES) {
-			strtok(filename, "\n"); // Remove new line character
-			if(filename[strlen(filename)] != '/') {
-				strcat(filename, "/");
-			}
-			strcat(filename, *cmd);
-			
-			if(stat(filename, &file_stat) == 0){
-				if(file_stat.st_mode & S_IXOTH) {
-					*cmd = filename;
-					printf("cmd: %s\n", *cmd);
-					return true;				
-				}		
-			}
-			n++;
-		} else if(n > MAX_NUM_DIRECTORIES) {
-			break;
-		} else {
-			n++;		
-		}
-	}
-	printf("Command: %s not recognized\nCheck it's path is in '.sh360rc'\n", *cmd);
-	return false;
-}
+/*************************** IMPLEMENTATION *****************************/
 
-bool get_full_path(char **cmd) {
-
-	if(*cmd[strlen(*cmd) - 1] == '\n') {
-		*cmd[strlen(*cmd) - 1] = '\0';
-	}
-
-	struct stat file_stat;
-	
-	FILE *rc;
-	rc = fopen(".sh360rc", "r");
-
-	char filename[MAX_LEN_PATH];
-	int n = 0;
-
-	while(fgets(filename, sizeof filename, rc) != NULL) {
-		if(n > 0 && n <= MAX_NUM_DIRECTORIES) {
-			strtok(filename, "\n"); // Remove new line character
-			if(filename[strlen(filename)] != '/') {
-				strcat(filename, "/");
-			}
-			strcat(filename, *cmd);
-			
-			if(stat(filename, &file_stat) == 0){
-				if(file_stat.st_mode & S_IXOTH) {
-					*cmd = filename;
-					printf("cmd: %s\n", *cmd);
-					return true;				
-				}		
-			}
-			n++;
-		} else if(n > MAX_NUM_DIRECTORIES) {
-			break;
-		} else {
-			n++;		
-		}
-	}
-	printf("Command: %s not recognized\nCheck it's path is in '.sh360rc'\n", *cmd);
-	return false;
-}
-
-void PP(char **token, int len) {
-	
-	int cmds[] = {1, -1, -1};
-	command_positions(cmds, token, len);
-	
-	if(cmds[1] == -1) {
-		printf("Missing '->' for pipe\n");
-		return;
-	}
-	if(cmds[1] == 2) {
-		printf("Missing command before '->'\n");
-		return;		
-	}
-	if(cmds[1] >= len || cmds[2] >= len) {
-		printf("Missing command after last '->'\n");
-		return;
-	}
-	if(cmds[2] - cmds[1] == 1) {
-		printf("Missing command between '->'  '->'\n");
-		return;
-	}
-	
-	bool multi = (cmds[2] != -1) ? true : false;
-	int num = (multi) ? 2 : 1;
-	int t;
-			
-	// Seperate arguments from commands
-	char *args_head[MAX_NUM_ARGS]; 
-	process_args(args_head, token, cmds[0]+1, cmds[1]-1);
-	
-	char *args_midd[MAX_NUM_ARGS];
-	if(multi) {
-		process_args(args_midd, token, (cmds[1]+1), (cmds[2]-1));
-		t = (cmds[2]+1);
-	}else{
-		t = (cmds[1]+1);
-	}
-	
-	char *args_tail[MAX_NUM_ARGS];
-	process_args(args_tail, token, t, len);
-	
-	// Commands to pass to execve
-	//char *cmd_head[] = {token[cmds[0]], *args_head, 0};
-	//char *cmd_tail[] = {token[cmds[num]] , *args_tail, 0};  
-	
-	char *test = "/bin/ls";
-	char *path_head = token[cmds[0]];
-	validate_command(&path_head);
-	char *test2 = path_head;
-
-	//printf("%d\n", strcmp(path_head, test));
-
-	char *cmd_head[] = {test2, *args_head, 0};
-	char *cmd_tail[] = {"/usr/bin/wc", *args_tail, 0}; 
-	char *cmd_midd[3];
-	
-	if(multi) {
-		cmd_midd[0] = token[cmds[1]];
-		cmd_midd[1] = *args_midd;
-		cmd_midd[2] = 0;	
-	}
-	
-	char *envp[] = { 0 };
-	
-	int status;
-	int pid_1, pid_2, pid_3;
-	int fd[2*num];
-
-	pipe(fd);
-	if(multi) { pipe(fd+2); }
-
-	printf("parent: setting up piped commands...\n");
-	if((pid_1 = fork()) == 0) {
-		printf("child (1): re-routing plumbing; STDOUT to pipe.\n");
-		dup2(fd[1], 1);
-		close(fd[0]);
-		execve(cmd_head[0], cmd_head, envp);
-		printf("child (1): SHOULDN'T BE HERE.\n");	
-	}
-	
-	if(multi) {
-		if((pid_2 = fork()) == 0) {
-			printf("child (2): re-routing plumbing; pipe to STDIN.\n");
-			dup2(fd[0], 0);
-			close(fd[1]);
-			printf("child (2): re-routing plumbing; STDOUT to pipe.\n");
-			dup2(fd[3], 1);
-			close(fd[2]);
-			execve(cmd_midd[0], cmd_midd, envp);
-			printf("child (2): SHOULDN'T BE HERE.\n");
-		}
-	}
-	
-	if((pid_3 = fork()) == 0) {
-		printf("child (3): re-routing plumbing; pipe to STDIN.\n");
-		if(multi) {
-			dup2(fd[2], 0);
-			close(fd[1]);
-			close(fd[3]);	
-		} else {		
-			dup2(fd[0], 0);
-			close(fd[1]);
-		}
-		execve(cmd_tail[0], cmd_tail, envp);
-		printf("child (3): SHOULDN'T BE HERE.\n");
-	}
-	
-	close(fd[0]);
-	close(fd[1]);
-	if(multi) {
-		close(fd[2]);
-		close(fd[3]);
-	}
-
-	printf("parent: waiting for child (1) to finish...\n");
-    waitpid(pid_1, &status, 0);
-    printf("parent: child (1) is finished.\n");
-	
-	if(multi) {
-		printf("parent: waiting for child (2) to finish...\n");
-		waitpid(pid_2, &status, 0);
-		printf("parent: child (2) is finished.\n");
-	}
-	
-    printf("parent: waiting for child (3) to finish...\n");
-    waitpid(pid_3, &status, 0); 
-    printf("parent: child (3) is finished.\n");
-
-}// End PP
-
-void OR(char *token[]) {
-	
-	char *args[MAX_NUM_ARGS];
-    char *envp[] = { 0 };
-    
-	int pid, fd;
-    int status;
-	int delim = -1;
-	int i = 0;
-	
-
-	if ((pid = fork()) == 0) {
-       
-		while(token[i] != NULL) {
-			if(strcmp(token[i], "->") == 0) {
-				delim = -1;
-			}
-			i++;
-		}
-		
-		if(delim < 0) {
-			fprintf(stderr, "OR missing '->'. Usage: OR <command> -> <destination>\n");
-			return;
-		}
-		if(token[delim+1] == NULL) {
-			fprintf(stderr, "OR missing destination. Usage: OR <command> -> <destination>\n");
-			return;
-		}
-		if(i<4) {
-			fprintf(stderr, "OR missing command. Usage: OR <command> -> <destination>\n");
-			return;
-		}
-			
-		int j = 0;
-		for(i = 1; i < delim; ++i) {
-			args[j] = token[i];
-			j++;
-		}
-		
-        fd = open(token[i+1], O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
-        if (fd == -1) {
-            fprintf(stderr, "cannot open %s for writing\n", token[delim+1]);
-            exit(1);
-        }
-
-		get_full_path(&args[0]);
-		 fprintf(stderr, "Fullpath: %s\n", args[0]);
-
-        dup2(fd, 1);
-        dup2(fd, 2); 
-        execve(args[0], args, envp);
-        printf("child: SHOULDN'T BE HERE.\n");
+void end_line(char * str) {
+	if (str[strlen(str) - 1] == '\n') {
+        str[strlen(str) - 1] = '\0';
     }
- 	printf("parent: waiting for child (3) to finish...\n");
-    waitpid(pid, &status, 0);
-	printf("parent: child is finished.\n");
-
-}// End OR
-
-int tokenize_string(char *s, char *arr[], char *delm, int num) {
-	
-	char *t;
-	int n = 0;
-
-	t = strtok(s, delm);
-
-	while(t != NULL && n < num) {
-		arr[n++] = t;
-		t = strtok(NULL, delm);
-	}
-	return n;
 }
 
-// https://stackoverflow.com/questions/15515088/how-to-check-if-string-starts-with-certain-string-in-c/15515276
-bool starts_with(const char *a, const char *b) {
+int read_config() {
+	
+	FILE *fp;
+	if(!(fp = fopen(".sh360rc", "r"))) {
+		fprintf(stderr, ".sh360rc not found");
+		return 0;
+	}
 
-	if(strncmp(a, b, strlen(b)) == 0)
-		return 1;
+	fgets(g_prompt, MAX_PROMPT_LENGTH, fp);
+	end_line(g_prompt);
+	
+	g_numDirs = 0;
+
+	while(fgets(g_dirs[g_numDirs], sizeof g_dirs[g_numDirs], fp) && g_numDirs < MAX_NUM_DIRS_IN_PATH) {
+		end_line(g_dirs[g_numDirs]);
+		g_numDirs++;
+	}
+	
+	return 1;
+}
+
+int find_path(char *binary, char *fullpath) {
+	return 1;
+}
+
+int tokenize_string(char **token, char *cmd) {
+	char *t;
+	int num_tokens = 0;
+
+	t = strtok(cmd, " ");
+	while(t != NULL && num_tokens < MAX_NUM_ARGS) {
+		token[num_tokens++] = t;
+        t = strtok(NULL, " ");
+	}
+
+	token[num_tokens] = NULL;
+
+	int i;
+    for(i = 0; i < num_tokens; i++) {
+		char *t = token[i];
+		if(strncmp(t, "~", 1) == 0) {
+			
+			char temp[strlen(t)+1];
+			memcpy(temp, t, strlen(t));
+			temp[strlen(t)] = '\0';
+
+			t[0] = '\0';
+			strcat(t, getenv("HOME"));
+			strcat(t, &temp[1]);
+    		strcat(t, "\0");
+
+		} else if(strncmp(t, "./", 2) == 0){
+			
+			char temp[strlen(t) + 1];
+    		memcpy(temp, t, strlen(t));
+    		temp[strlen(t)] = '\0';
+			
+			// ref[1]
+			char cwd[1024];
+    		getcwd(cwd, sizeof(cwd));
+
+    		t[0] = '\0';
+    		strcat(t, cwd);
+    		strcat(t, &temp[1]);
+    		strcat(t, "\0");
+		}
+	}
+	return num_tokens;
+}
+
+int piping(char *cmd) {
+	
+	char *token[MAX_NUM_ARGS];
+	int num_tokens = tokenize_string(token, cmd);
+
+	int delim_1 = -1;
+	int delim_2 = -1;
+	int i = 0;
+
+	while(token[i] != NULL) {
+		if(strcmp(token[i], "->") == 0){
+			if(delim_1 == -1)
+				delim_1 = i;
+			else
+				delim_2 = i;
+		}
+		i++;
+	}
+	
+	int multi = (delim_2 != -1) ? 2 : 1;
+
+	if(delim_1 == -1) {
+		fprintf(stderr, "PP missing '->'. Usage: PP <command> -> <command>\n");
+		return 0;
+	}
+	if(delim_1 == 2) {
+		fprintf(stderr, "PP missing <command> before '->'. Usage: PP <command> -> <command>\n");
+		return 0;
+	}
+	if(delim_1 >= num_tokens || delim_2 >= num_tokens) {
+		fprintf(stderr, "PP missing <command> after last '->'. Usage: PP <command> -> <command> (-> <command>)\n");
+		return 0;
+	}
+	if(delim_2 - delim_1 == 1 && multi == 2) {
+		fprintf(stderr, "PP missing <command> between '->'. Usage: PP <command> -> <command> -> <command>\n");
+		return 0;
+	}
+
+	if(multi == 1)
+		delim_2 = num_tokens;
+	
+	int j;
+	
+	char *token_1[delim_1-1];
+	for(j = 1; j < delim_1; j++) {
+		token_1[j-1] = token[j];
+	}
+	
+	char *token_2[delim_2-delim_1];
+	i = 0;
+	for(j = delim_1+1; j < delim_2; j++)
+		token_2[i++] = token[j];
+	
+	char *token_3[num_tokens-delim_2];
+	i = 0;
+	if(multi == 2){
+		for(j = delim_2+1; j < num_tokens; j++)
+			token_3[i++] = token[j];
+	}
+	
+	char binary_1[MAX_LINE_LENGTH];
+	char binary_2[MAX_LINE_LENGTH];
+	char binary_3[MAX_LINE_LENGTH];
+
+	int find_path(token_1[0], binary_1);
+	int find_path(token_2[0], binary_2);
+	int find_path(token_3[0], binary_3);
+		
+	
 	return 0;
 }
 
+int output_redirect(char *cmd) {
+	return 0;
+}
 int main(int argc, char *argv[]) {
-	
-	char input[MAX_INPUT_LINE];
-	char *token[MAX_NUM_TOKENS];
-	char prompt[MAX_LEN_PROMT];
-	char delim[] = " ";
 
-	int num_tokens;
-	int i = 0;
+	read_config();
 
-	FILE *rc;
-	if(!(rc = fopen(".sh360rc", "r"))) {
-		printf(".sh360rc not found");
-		exit(1);
-	}
-	
-	fscanf(rc, "%s", prompt);	
-	fclose(rc);
-	
 	for(;;) {
-
-		fprintf(stdout, "%s", prompt);
+		
+		fprintf(stdout, "%s ", g_prompt);
 		fflush(stdout);
-		fgets(input, MAX_INPUT_LINE, stdin);
+	
+		char input[MAX_LINE_LENGTH];
 		
-		if(input[strlen(input) - 1] == '\n') {
-			input[strlen(input) - 1] = '\0';
-		}
-		
-		// Exit
-		if(strcmp(input, "exit") == 0) {
-			break;		
-		}
+		fgets(input, MAX_LINE_LENGTH, stdin);
+		end_line(input);
 
-		num_tokens = tokenize_string(input, token, delim, MAX_NUM_TOKENS);
-
-		if(strcmp(token[0], "PP") == 0) {
-			PP(token, num_tokens);			
-		} 
-		else if(strcmp(token[0], "OR") == 0) {
-			OR(token);
-		}
-		else {
-			printf("Error: %s - Command not recognized\n", token[0]); 		
-		}
-	}// End For	
-	return(0);
-}// End main
-
-
+		if(strcmp(input, "exit") == 0)
+			exit(0);
+		if(strncmp(input, "PP", 2) == 0)
+			piping(input);
+		if(strncmp(input, "OR", 2) == 0)
+			output_redirect(input);
+	}
+	return 0;
+}
