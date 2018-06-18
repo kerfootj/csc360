08e02c92f28dddf4ae2fdc4e60298434652aa5cb
kerfootj <joelkerfoot@gmail.com>
2018-06-18 12:54:48 -0700

    reuseable barrier imlimented - needs testing

--- a2/meetup.c
+++ a2/meetup.c
@@ -12,6 +12,14 @@
  * Declarations for barrier shared variables -- plus concurrency-control
  * variables -- must START here.
  */
+int num;
+int meet;
+int count;
+resource_t r;
+sem_t t1;
+sem_t t2;
+sem_t mutex;
+
 void initialize_meetup(int n, int mf) {
     char label[100];
     int i;
@@ -26,9 +34,61 @@ void initialize_meetup(int n, int mf) {
      * Initialize the shared structures, including those used for
      * synchronization.
      */
+
+    num = n;
+    meet = mf;
+    count = 0;
+
+    init_resource(&r, "TEMP NAME");
+
+    if (sem_init(&t1, 0, 0) != 0) {
+    	fprintf(stderr, "Failed to initialize semaphore: t1\n");
+    	exit(1);
+    }
+    if (sem_init(&t2, 0, 1) != 0) {
+    	fprintf(stderr, "Failed to initialize semaphore: t2\n");
+    	exit(1);
+    }
+    if (sem_init(&mutex, 0, 1) != 0) {
+    	fprintf(stderr, "Failed to initialize semaphore: mutex\n");
+    	exit(1);
+    }
+
 }
 
 
 void join_meetup(char *value, int len) {
-    printf("NOTHING IMPLEMENTED YET FOR join_meetup\n");
+    
+
+    sem_wait(&mutex);
+
+    if (count == 0 && meet == MEET_FIRST) { write_resource(&r, value, len); }
+
+    if (++count == num) {
+    	
+    	if (meet == MEET_LAST) { write_resource(&r, value, len); }
+
+    	sem_wait(&t2);
+    	sem_post(&t1);
+    }
+
+    sem_post(&mutex);
+
+    sem_wait(&t1);
+    sem_post(&t1);
+
+    sem_wait(&mutex);
+
+    read_resource(&r, value, len);
+
+    if (--count == 0) {
+    	sem_wait(&t1);
+    	sem_post(&t2);
+
+    }
+
+    sem_post(&mutex);
+   	sem_wait(&t2);
+    sem_post(&t2);
+
 }
