45de91cf6b832fca5668712b85b7a736d16043fa
kerfootj <joelkerfoot@gmail.com>
2018-06-20 12:47:00 -0700

    final commit: implimented reusable barriers

--- a2/meetup.c
+++ a2/meetup.c
@@ -12,13 +12,17 @@
  * Declarations for barrier shared variables -- plus concurrency-control
  * variables -- must START here.
  */
+
 int num;
-int meet;
 int count;
+int meet;
+
 resource_t r;
+
 sem_t t1;
 sem_t t2;
 sem_t mutex;
+sem_t barrier;
 
 void initialize_meetup(int n, int mf) {
     char label[100];
@@ -53,42 +57,62 @@ void initialize_meetup(int n, int mf) {
     	fprintf(stderr, "Failed to initialize semaphore: mutex\n");
     	exit(1);
     }
-
+     if (sem_init(&barrier, 0, num) != 0) {
+    	fprintf(stderr, "Failed to initialize semaphore: barrier\n");
+    	exit(1);
+    }
 }
 
-
+// http://greenteapress.com/semaphores/LittleBookOfSemaphores.pdf
 void join_meetup(char *value, int len) {
     
+    // Wait for current meetup to finish if full
+    sem_wait(&barrier);
 
-    sem_wait(&mutex);
+    /* Section 1 */
+    sem_wait(&mutex); 
 
-    if (count == 0 && meet == MEET_FIRST) { write_resource(&r, value, len); }
+    // Add waiting thread
+    count += 1;
 
-    if (++count == num) {
+    // If MEET_FIRST save the first thread's keyword 
+    if (count == 1 && meet == MEET_FIRST) { write_resource(&r, value, len); }
+  	
+  	// Once meetup is full release threads
+   	if (count == num) {
     	
+    	// If MEET_LAST save the last thread's keyword
     	if (meet == MEET_LAST) { write_resource(&r, value, len); }
 
-    	sem_wait(&t2);
-    	sem_post(&t1);
+    	sem_wait(&t2); // Unlock section 1
+    	sem_post(&t1); // Lock section 2
     }
-
     sem_post(&mutex);
+    /* End Section 1 */
 
+    // Wait for meetup to fill
     sem_wait(&t1);
     sem_post(&t1);
 
-    sem_wait(&mutex);
+    /* Section 2 */
+    sem_wait(&mutex); 
 
+    // Remove thread from waiting and sync keyword
+    count -= 1;
     read_resource(&r, value, len);
 
-    if (--count == 0) {
-    	sem_wait(&t1);
-    	sem_post(&t2);
-
+    if (count == 0) {
+    	sem_wait(&t1); // Lock section 1
+    	sem_post(&t2); // Unlock section 2
     }
 
     sem_post(&mutex);
+    /* End Section 2*/
+
+    // Sync threads release
    	sem_wait(&t2);
     sem_post(&t2);
 
+    // Allow next batch to begin meetup
+    sem_post(&barrier);
 }
